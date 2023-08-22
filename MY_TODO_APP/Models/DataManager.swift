f4b5ff7e883df20dc8ee4472617abeb3fb8a771c
jakkujakku <jakkujakku98@gmail.com>
2023-08-22 21:16:48 +0900

    폴더 생성 및 분류

---
+++ MY_TODO_APP/Models/DataManager.swift
@@ -0,0 +1,8 @@
+//
+//  DataManager.swift
+//  MY_TODO_APP
+//
+//  Created by (^ㅗ^)7 iMac on 2023/08/22.
+//
+
+import Foundation
---
+++ MY_TODO_APP/Models/MediaService.swift
@@ -0,0 +1,8 @@
+//
+//  MediaService.swift
+//  MY_TODO_APP
+//
+//  Created by (^ㅗ^)7 iMac on 2023/08/22.
+//
+
+import Foundation
